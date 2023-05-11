6a323e07d236a6b50f42855a4649b8a693c5ed38
wguilherme <wguilhermecriativo@gmail.com>
2023-05-11 11:47:03 -0300

    feat: isolate components by module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PoModule } from '@po-ui/ng-components';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { ButtonComponent } from './components/atoms/button/button.component';
+import { AtomsModule } from './components/atoms/atoms.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ButtonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PoModule,
     HttpClientModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    AtomsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
